b89aaf26fbfe1aec52a96b720153c23d15fd21a3
Renat <clarkin200@gmail.com>
2024-08-26 00:05:02 +0300

    Innit commit

---
+++ src/com/gmail/clarkin200/Main.java
@@ -0,0 +1,18 @@
+package com.gmail.clarkin200;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("App for temperature converting");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter celsius value to fahrenheit");
+        double celsius = sc.nextDouble();
+        System.out.println("Enter fahrenheit value to celsius");
+        double fahrenheit = sc.nextDouble();
+
+    }
+}
